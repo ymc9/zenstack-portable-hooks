c64e247bd002ecf1142d9199bea09af2ad713e1d
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-11-15 09:12:47 -0800

    initial commit

---
+++ client/src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider as ZenStackProvider } from './hooks/index.ts';
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <ZenStackProvider
+                value={{ endpoint: import.meta.env.VITE_API_ENDPOINT }}
+            >
+                <App />
+            </ZenStackProvider>
+        </QueryClientProvider>
+    </StrictMode>
+);
